5ec6b0587023d152ba501d96d1058d15c230ae77
pear_stack <grusza.adrian@gmail.com>
2024-03-07 13:35:45 +0100

    add meta-rpimachines, update build script, update meta-mui

--- do_image.sh
+++ do_image.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
-IMAGE="my-qt5-image"
-IMAGE_DIR="build/tmp/deploy/images/raspberrypi4"
+export MACHINE="r4b"
+export IMAGE="mui-base-image"
+export VERSION=$(date +"%d%m%y_%H%M")
 
-. layers/poky/oe-init-build-env
+for arg in "$@"; do
+	name=${arg%%=*}
+	value=${arg#*=}
+	case $name in
+	#"image") export IMAGE="${value}" ;;
+	"machine") export MACHINE="${value}" ;;
+	esac
+	shift
+done
 
+case $MACHINE in
+"r4b") IMAGE="mui-base-image" ;;
+"r02w") IMAGE="mui-remote-image" ;;
+esac
+
+echo "Building"
+echo "MACHINE: $MACHINE"
+echo "IMAGE: $IMAGE"
+echo "VERSION: $VERSION"
+export IMAGE_DIR="build/tmp-glibc/deploy/images/${MACHINE}"
+
+source layers/poky/oe-init-build-env
 bitbake $IMAGE
-cp ../${IMAGE_DIR}/${IMAGE}-raspberrypi4.wic.bz2 ../artifacts/
-bzip2 -d -f ../artifacts/${IMAGE}-raspberrypi4.wic.bz2
-mv ../artifacts/${IMAGE}-raspberrypi4.wic ../artifacts/dev-image.rpi-sdimg
+
+echo "Deploying $IMAGE image"
+cp ../${IMAGE_DIR}/${IMAGE}-${MACHINE}.wic.bz2 ../artifacts/
+bzip2 -d -f ../artifacts/${IMAGE}-${MACHINE}.wic.bz2
+mv ../artifacts/${IMAGE}-${MACHINE}.wic ../artifacts/${IMAGE}-${VERSION}.rpi-sdimg
